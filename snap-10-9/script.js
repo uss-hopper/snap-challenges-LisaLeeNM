c23925c2d8855014a16e3bc1bde804ad70c3e17e
Lisa Lee <llee28@cnm.edu>
2019-10-09 16:12:39 -0600

    created two different functions

---
+++ snap-10-9/script.js
@@ -0,0 +1,16 @@
+/** This function multiplies two numbers. **/
+
+function multNum(param1,param2) {
+	return param1*param2;
+}
+
+console.log(multNum(3,7));
+
+
+/** This function takes two strings as arguments and returns a value that has them concatenated. **/
+
+function concatStrings(firstName, lastName) {
+	return firstName + " " + lastName;
+}
+
+//console.log(concatStrings('Lisa', 'Lee'));
