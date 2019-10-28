7cb790d550eb140e1df6d508b9c3ee70312cd9fa
Lisa Lee <llee28@cnm.edu>
2019-10-28 08:29:46 -0600

    add accessor and mutator for tweet content

--- snap-10-28/Tweet.php
+++ snap-10-28/Tweet.php
@@ -10,5 +10,26 @@
 }
 
 	/**
-	 * mutator method for tweet
+	 * mutator method for tweet content
+	 *
+	 * @param string $newTweetContent new value of tweet content
+	 * @throws \InvalidArgumentException if the $newTweetContent is not a string or insecure
+	 * @throws \RangeException if $newTweetContent is > 140 characters
+	 * @throws \TypeError if $newTweetContent is not a string
 	 **/
+	public function setTweetContent(string $newTweetContent) : void {
+		// verify the tweet content is secure
+		$newTweetContent = trim($newTweetContent);
+		$newTweetContent = filter_var($newTweetContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newTweetContent) === true) {
+			throw(new \InvalidArgumentException("tweet content is empty or insecure"));
+		}
+
+		// verify the tweet content will fit in the database
+		if(strlen($newTweetContent) > 140) {
+			throw(new \RangeException("tweet content too large"));
+		}
+
+		// store the tweet content
+		$this->tweetContent = $newTweetContent;
+}
