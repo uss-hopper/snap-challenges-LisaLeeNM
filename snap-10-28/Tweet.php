e55dca9610365533220f44f511c8a1b93152f810
Lisa Lee <llee28@cnm.edu>
2019-10-28 08:22:05 -0600

    snap challenge 10-28

---
+++ snap-10-28/Tweet.php
@@ -0,0 +1,14 @@
+<?php
+
+	/**
+	 * accessor method for tweet content
+	 *
+	 * @return string value of tweet content
+	 **/
+	public function getTweetContent() : string {
+		return($this->tweetContent);
+}
+
+	/**
+	 * mutator method for tweet
+	 **/
