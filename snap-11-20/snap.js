6eb38724c85cbd90f0deb961fa872fa46bd8b3ca
Lisa Lee <llee28@cnm.edu>
2019-11-20 08:32:34 -0700

    try another method

--- snap-11-20/snap.js
+++ snap-11-20/snap.js
@@ -1,10 +1,14 @@
-Input: Array.of(2, 82, 5, 2, 2, 14, 76, 82, 14)
-Input: Array.of.Sort(2, 2, 2, 5, 14, 14, 76, 82, 82)
-Output: ArrayOfUniqueNumbers(2, 5, 14, 76, 82)
-Output: ArrayOfCountsForUniqueNumbers(3,1,2,1,2)
+// Input: (2, 82, 5, 2, 2, 14, 76, 82, 14)
+// Input Sorted: (2, 2, 2, 5, 14, 14, 76, 82, 82)
+// Output Of Unique Numbers: (2, 5, 14, 76, 82)
+// Output Counts For Unique Numbers: (3,1,2,1,2)
 
-function numbers(arr) [{
-	2, 82, 5, 2, 2, 14, 76, 82, 14}];
+var arr = [2, 82, 5, 2, 2, 14, 76, 82, 14];
+var counts = {};
 
-	arr.sort();
-	for (var i = 0; i < arr.length; i++)
+for (var i = 0; i < arr.length; i++) {
+	var num = arr[i];
+	counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+
+console.log(counts[2], counts[5], counts[14], counts[76], counts[82]);
