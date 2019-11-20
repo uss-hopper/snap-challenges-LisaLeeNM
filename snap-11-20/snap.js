b179dd4f6cab78a8abbe001e6519b02a18266fb6
Lisa Lee <llee28@cnm.edu>
2019-11-20 08:48:58 -0700

    start function on output for numbers between 2 integers (inclusive)

--- snap-11-20/snap.js
+++ snap-11-20/snap.js
@@ -1,14 +1,26 @@
+// In pseudocode, write a function that takes an array of numbers, and returns another array that counts the number of times each number appears in the array.
 // Input: (2, 82, 5, 2, 2, 14, 76, 82, 14)
 // Input Sorted: (2, 2, 2, 5, 14, 14, 76, 82, 82)
 // Output Of Unique Numbers: (2, 5, 14, 76, 82)
 // Output Counts For Unique Numbers: (3,1,2,1,2)
 
-var arr = [2, 82, 5, 2, 2, 14, 76, 82, 14];
-var counts = {};
+// var arr = [2, 82, 5, 2, 2, 14, 76, 82, 14];
+// var counts = {};
+//
+// for (var i = 0; i < arr.length; i++) {
+// 	var num = arr[i];
+// 	counts[num] = counts[num] ? counts[num] + 1 : 1;
+// }
+//
+// console.log(counts[2], counts[5], counts[14], counts[76], counts[82]);
 
-for (var i = 0; i < arr.length; i++) {
-	var num = arr[i];
-	counts[num] = counts[num] ? counts[num] + 1 : 1;
+// In pseudocode, write a function that takes two integers. For each integer in between those two numbers (inclusive), output 'fizz' if the number is even, 'buzz' if the number is divisible by 3, and 'baz' without 'fizz' or 'buzz' if the number is divisible by both.
+// Two integers
+// Integer a = 1
+// Integer b = 10
+for(var i=1; i<11; i++) {
+	if( (i%2) === 1 ) {
+		console.log("fizz");
+	}else if( (i%2) === )
+	}
 }
-
-console.log(counts[2], counts[5], counts[14], counts[76], counts[82]);
