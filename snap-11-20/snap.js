dec606e199e8407756c53bef74aa79ca47f58380
Lisa Lee <llee28@cnm.edu>
2019-11-20 08:23:18 -0700

    attempt to add sort fxn

--- snap-11-20/snap.js
+++ snap-11-20/snap.js
@@ -1,3 +1,10 @@
-Input: Array.of(2, 2, 2, 5, 14, 14, 76, 82, 82)
+Input: Array.of(2, 82, 5, 2, 2, 14, 76, 82, 14)
+Input: Array.of.Sort(2, 2, 2, 5, 14, 14, 76, 82, 82)
 Output: ArrayOfUniqueNumbers(2, 5, 14, 76, 82)
 Output: ArrayOfCountsForUniqueNumbers(3,1,2,1,2)
+
+function numbers(arr) [{
+	2, 82, 5, 2, 2, 14, 76, 82, 14}];
+
+	arr.sort();
+	for (var i = 0; i < arr.length; i++)
