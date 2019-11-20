3864f9bc66d0c50bb849d433c9e7f80c22058994
Lisa Lee <llee28@cnm.edu>
2019-11-20 10:03:55 -0700

    include Marty's code

--- snap-11-20/snap.js
+++ snap-11-20/snap.js
@@ -14,18 +14,39 @@
 //
 // console.log(counts[2], counts[5], counts[14], counts[76], counts[82]);
 
+// Marty's code
+function counts ($array){
+
+	$newArray = [];
+
+	foreach( $array as $element){
+		if (isset($newArray[$element])){
+		$newArray[$element] = $newArray[$element] + 1]; // or ++
+		}else{
+			$newArray[$element] = 1;
+		};
+
+		echo($element . " " . $newArray[$element] . PHP_EOL);
+	};
+	return $newArray;
+};
+
+$origArray = [1,1,1,2,3,3,4,4,4,4];
+var_dump(counts($origArray));
+
 // In pseudocode, write a function that takes two integers. For each integer in between those two numbers (inclusive), output 'fizz' if the number is even, 'buzz' if the number is divisible by 3, and 'baz' without 'fizz' or 'buzz' if the number is divisible by both.
 // Two integers
 // Integer a = 1
 // Integer b = 10
-for(var i=1; i<11; i++) {
-	if( (i%2) === 0 ) {
-		console.log("fizz");
-	}else if( (i%3) === 0) {
-		console.log("buzz");
-	}else if( (i%2) === 0 && (i%3) === 0) {
-		console.log("baz");
-	}else {
-		console.log(i);
-	}
-}
+// for(var i=1; i<11; i++) {
+// 	if( (i%2) === 0 ) {
+// 		console.log("fizz");
+// 	}else if( (i%3) === 0) {
+// 		console.log("buzz");
+// 	}else if( (i%2) === 0 && (i%3) === 0) {
+// 		console.log("baz");
+// 	}else {
+// 		console.log(i);
+// 	}
+// }
+
