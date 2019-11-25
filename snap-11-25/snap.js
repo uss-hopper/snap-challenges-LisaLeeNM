9077987083ac1dab0d92b6c85bf36cd3a1d0a306
Lisa Lee <llee28@cnm.edu>
2019-11-25 08:25:10 -0700

    complete first question

---
+++ snap-11-25/snap.js
@@ -0,0 +1,21 @@
+// In pseudocode, write a function that takes a string and returns it with all vowels (not including y) removed.
+
+function removeVowels (start, end){
+	Result = ""
+	For each character between start and end {
+		if character === "a" {
+				Result = "" }
+		if character === "e" {
+				Result = "" }
+		if character === "i" {
+				Result = "" }
+		if character === "o" {
+				Result = "" }
+		if character === "u" {
+			Result = "" }
+		Print Result
+	}
+
+
+// In pseudocode, write a function that takes an array of numbers and returns the greatest number in the array.
+
