e8ac4bbbadd4fb18cf7f618f90ff8d2031cc567a
Lisa Lee <llee28@cnm.edu>
2019-11-27 08:49:51 -0700

    add comments

--- snap-11-27/snap.js
+++ snap-11-27/snap.js
@@ -14,8 +14,10 @@ console.log(arrayNum.reduce(reducer));
 
 const arrayWords = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."]
 
-let mystring = arrayWords.join('');
+// concatenate words together while removing commas
+const newArrayWords = arrayWords.join('');
 
-console.log(arrayWords.join(''));
+// remove period in the new string
+const newString = newArrayWords.split('.').join('');
 
-
+console.log(newString);
