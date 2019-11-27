46d59b7d0f9913c59da168eedda794c36653b308
Lisa Lee <llee28@cnm.edu>
2019-11-27 09:32:17 -0700

    add Brent's soln

--- snap-11-27/snap.js
+++ snap-11-27/snap.js
@@ -7,6 +7,15 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 console.log(arrayNum.reduce(reducer));
 
+// Brent's solution
+
+// const arrayNum = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
+//
+// let sum = arrayNum.reduce(function(accumulator, currentValue) {
+// 	return accumulator + currentValue;
+// });
+//
+// console.log("The sum of the array is " + sum);
 
 // Turn this array into a string using JavaScript reduce.
 
@@ -21,3 +30,13 @@ const newArrayWords = arrayWords.join('');
 const newString = newArrayWords.split('.').join('');
 
 console.log(newString);
+
+
+// Brent's solution
+
+const wordArray = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."]
+
+const addWords = (text, currentValue) => text + ' ' + currentValue;
+
+console.log(wordArray.reduce(addWords));
+
