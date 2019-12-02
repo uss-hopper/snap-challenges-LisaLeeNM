70bc0bfa2cfb4e95082c4de6915eda0f42e7dd32
Lisa Lee <llee28@cnm.edu>
2019-12-02 08:59:51 -0700

    try another approach

--- snap-12-2/snap.js
+++ snap-12-2/snap.js
@@ -1,6 +1,7 @@
 // Map this array of text to the DOM (Hint: Each array element must appear in its own html tag in the browser)
 const names = ['Bob', 'Jim', Matt', 'Mary', 'Suzy', ' Sarah'];
 
+// First attempt
 // Get the element
 const app = document.querySelector('#app');
 
@@ -9,3 +10,19 @@ app.innerHTML = '<h1>' + names.map(function (name) {
 	return '<h1>' + name + '</h1>>'; // transforms array content
 });
 
+console.log(app);
+
+
+// Second attempt
+import React from "react";
+import ReactDOM from "react-dom";
+
+const names = ['Bob', 'Jim', Matt', 'Mary', 'Suzy', ' Sarah'];
+const Nameslist = () => (
+	<div>
+		{names.map(name => <h1 key={name}> {name} </h1>)}
+	</div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Nameslist />, rootElement);
