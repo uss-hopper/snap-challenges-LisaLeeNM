9770beb32e5413f109c687d1287245799bef7432
Lisa Lee <llee28@cnm.edu>
2019-12-02 08:22:44 -0700

    create markup

--- snap-12-2/snap.js
+++ snap-12-2/snap.js
@@ -1,3 +1,11 @@
-names.map(function(name) {
-	return '<h1>' + name + '</h1>h1>'; // transforms array content
-})
+// Map this array of text to the DOM (Hint: Each array element must appear in its own html tag in the browser)
+const names = ['Bob', 'Jim', Matt', 'Mary', 'Suzy', ' Sarah'];
+
+// Get the element
+const app = document.querySelector('#app');
+
+// Create markup
+app.innerHTML = '<h1>' + names.map(function (name) {
+	return '<h1>' + name + '</h1>>'; // transforms array content
+});
+
